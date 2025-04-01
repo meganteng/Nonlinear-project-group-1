56269f668532471040967f5f08fcc9563028e71a
svemyh <s-myhre@outlook.com>
2025-03-31 21:15:10 -0700

    add fminsearch results LQR_full_feedback

--- tune_fminsearch_LQR_full_feedback.m
+++ tune_fminsearch_LQR_full_feedback.m
@@ -12,3 +12,13 @@ options = optimset('Display','iter','TolX',1e-2,'TolFun',1e-2);
 fprintf('Optimal Parameters: Q=[%.2f, %.2f, 0.00, 0.00], R=%.2f, k_servo=%.2f, nl_scale=%.2f\n',...
     opt_params(1), opt_params(2), opt_params(3), opt_params(4), opt_params(5));
 fprintf('Best Score: %.4f\n', best_score);
+
+// Parameters: Q=[158.10, 0.39, 0.00, 0.00], R=0.03, k_servo=7.24, nl_scale=1.30, Score=2.9715
+//    107          209          2.97147         reflect
+// Average Tracking Error: 0.0013 
+// Average Energy Consumption: 0.1380 
+// Safety Contraint Violation: 0 
+// Tracking Cost: 2.28 
+// Energy Cost: 0.69 
+// Safety Cost: 0.00 
+// Total Score: 2.97 
