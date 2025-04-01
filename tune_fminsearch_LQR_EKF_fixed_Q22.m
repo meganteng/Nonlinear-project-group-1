4c39083033829105ccfe7134d6a07e8a83774723
svemyh <s-myhre@outlook.com>
2025-03-31 22:15:40 -0700

    add fminsearch schema LQR_EKF_fixed_Q22

---
+++ tune_fminsearch_LQR_EKF_fixed_Q22.m
@@ -0,0 +1,15 @@
+% FILE: tune_fminsearch_LQR_EKF.m
+
+% Define an initial guess for [Q11, R, obs_factor]
+p0 = [300, 0.4, 2];
+
+% Set optimization options (display iterations, tolerance, etc.)
+options = optimset('Display','iter','TolX',1e-2,'TolFun',1e-2);
+
+% Optimize the objective function (minimizing score)
+[opt_params, best_score] = fminsearch(@tune_objective, p0, options);
+
+fprintf('Optimal Parameters: Q11=%.2f, Q22=%.2f, R=%.2f, obs_factor=%.2f\n',...
+    opt_params(1), 0, opt_params(2), opt_params(3));
+fprintf('Best Score: %.4f\n', best_score);
+
